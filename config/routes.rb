00c986e2f1dd0e014b4a1f559e70982209d26e2d
Georgina Daniell <georgina.daniell@deliveroo.co.uk>
2019-04-16 22:34:24 +0100

    Create homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
   post '/where_to_meet', controller: :where_to_meet, action: :create
 end
