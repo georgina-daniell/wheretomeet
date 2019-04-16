77c4e6d6bd9cecf8d10592e730bfdf29f165470a
Georgina Daniell <georgina.daniell@deliveroo.co.uk>
2019-04-16 23:00:00 +0100

    Create simple ui for testing

--- app/controllers/where_to_meet_controller.rb
+++ app/controllers/where_to_meet_controller.rb
@@ -1,9 +1,22 @@
 class WhereToMeetController < ApplicationController
   def create
-    render json: Main.new.call(params), status: :ok
+    # render plain: params[:where_to_meet].inspect
+    if locations_params
+      render json: Main.new.call(locations_params), status: :ok
+    else
+      render json: {}, status: :bad_request
+    end
   end
 
-  def params
-    params.require(:locations)
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def locations_params
+    params[:where_to_meet].require(:locations).split(',')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
-  post '/where_to_meet', controller: :where_to_meet, action: :create
+  resources :where_to_meet
 end
