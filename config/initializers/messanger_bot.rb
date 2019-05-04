27cb20333de239b0f1753fd0690145489907a85c
Georgina Daniell <georgina.daniell@deliveroo.co.uk>
2019-05-04 17:34:36 +0100

    Revert "User latest ruby version for reloader"
    This reverts commit e133cde808635e8952bbcd28b4c0100158817a97.

--- config/initializers/messanger_bot.rb
+++ config/initializers/messanger_bot.rb
@@ -3,7 +3,7 @@ unless Rails.env.production?
   bots_reloader = ActiveSupport::FileUpdateChecker.new(bot_files) do
     bot_files.each{ |file| require_dependency file }
   end
-  ActiveSupport::Reloader.to_prepare do
+  ActionDispatch::Callbacks.to_prepare do
     bots_reloader.execute_if_updated
   end
   bot_files.each { |file| require_dependency file }
