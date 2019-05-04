5713c15b15f228e693fc88eca98a2232f101936d
Georgina Daniell <georgina.daniell@deliveroo.co.uk>
2019-05-04 17:35:27 +0100

    Revert "Add messenger bot"
    This reverts commit 69b49ce8f5b5b6b4db344bbccde31147316247c4.

--- app/bot/listen.rb
+++
@@ -1,6 +0,0 @@
-require "facebook/messenger"
-include Facebook::Messenger
-Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
-Bot.on :message do |message|
-  puts "got your message!"
-end
